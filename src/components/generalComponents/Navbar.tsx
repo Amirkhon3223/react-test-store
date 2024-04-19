b61b3e5cbf10015e56efd888852d88aeaa3e2ef3
Amirkhon3223 <amirich3223@outlook.com>
2024-04-19 05:04:53 +0500

    [DEV] - Dark Mode added

--- src/components/generalComponents/Navbar.tsx
+++ src/components/generalComponents/Navbar.tsx
@@ -1,14 +1,25 @@
-import { BiShoppingBag } from "react-icons/bi";
 import { useEffect, useState } from "react";
-import { openToggleCard } from "../../redux/slice/ProductCartSlice.tsx";
+import { BiMoon, BiShoppingBag, BiSun } from "react-icons/bi";
+import { openToggleCard } from "../../redux/slice/ProductCartSlice";
 import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "../../redux/Store.tsx";
+import { RootState } from "../../redux/Store";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state: RootState) => state.card);
   const [scroll, setScroll] = useState(false);
+
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    if (darkTheme) {
+      root.classList.add('dark');
+    } else {
+      root.classList.remove('dark');
+    }
+  }, [darkTheme]);
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -21,15 +32,28 @@ export const Navbar = () => {
       <div
         className={`${scroll ? "bg-grey shadow-md" : "shadow-none"} fixed top-0 left-0 w-full z-20 transition duration-300`}>
         <div className="flex items-center justify-between container py-4 px-2 mx-auto text-gray-800">
-          <Link to="/" className="font-bold text-xl">
+          <Link to="/"
+                className={`${scroll ? "dark:text-transparentDark" : "text-grey"} font-bold text-xl dark:text-grey text-transparentDark`}>
             Store-Test
           </Link>
-          <div className="relative cursor-pointer" onClick={() => dispatch(openToggleCard())}>
-            <BiShoppingBag className="text-2xl"/>
-            <div className="absolute w-4 h-4 rounded-full z-20 right-[-3px] bottom-[-3px]
-                            flex items-center justify-center text-[10px] bg-black text-white"
-            >
-              {cartItems.reduce((total, item) => total + item.amount, 0)}
+          <div className="flex items-center justify-between space-x-10">
+            <div className="relative cursor-pointer" onClick={() => dispatch(openToggleCard())}>
+              <BiShoppingBag
+                className={`${scroll ? "dark:text-transparentDark" : "text-grey"} text-3xl dark:text-grey`}/>
+              <div className={`${scroll ? "dark:text-grey" : "text-transparentDark"} absolute w-4 h-4 rounded-full z-20 right-[-3px] bottom-[-3px] flex
+                            items-center justify-center text-[10px] bg-black text-white dark:text-grey`}
+              >
+                {cartItems.reduce((total, item) => total + item.amount, 0)}
+              </div>
+            </div>
+            <div>
+              <button onClick={() => setDarkTheme(!darkTheme)}
+                      className={`${scroll ? "dark:text-transparentDark" : "text-grey"} dark:text-grey`}>
+                {
+                  darkTheme ?
+                    <BiSun className="inline mr-2 text-3xl"/> : <BiMoon className="inline mr-2 text-3xl"/>
+                }
+              </button>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Включение темной темы через классы
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
